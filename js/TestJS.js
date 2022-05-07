e768e4a6d238e4b921421ced0ca2a99793452d92
Valeriy <valer886@gmail.com>
2022-05-07 15:02:09 +0300

    07.05

--- js/TestJS.js
+++ js/TestJS.js
@@ -1,22 +1,89 @@
-const list = document.createElement("ul");
-const info = document.createElement("p");
-const html = document.querySelector("html");
+function getMathResult(base, progress){
+  if (progress === 0 || progress <= 0 || typeof(progress) !== "number"){
+    console.log("error");
+    return base;
+  }
 
-info.textContent =
-  "Below is a dynamic list. Click anywhere on the page to add a new list item. Click an existing list item to change its text to something else.";
+  let sett = "";
+  for (let i = 1; i <= progress; i++){
+    sett += `${base*i}---`;
+  }
+  sett = sett.slice(0,-3);
+  return sett;
+ }
 
-document.body.appendChild(info);
-document.body.appendChild(list);
 
-html.onclick = function () {
-  const listItem = document.createElement("li");
-  const listContent = prompt("What content do you want the list item to have?");
-  listItem.textContent = listContent;
-  list.appendChild(listItem);
 
-  listItem.onclick = function (e) {
-    e.stopPropagation();
-    const listContent = prompt("Enter new content for your list item");
-     this.textContent = listContent;
-  };
-};
+console.log(getMathResult(10, 5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const lines = 3;
+// let result = '';
+// let arr = [];
+// let o = 0;
+
+
+// for (let f = 0; f <= lines * 2; f++){ 
+//  if (f%2 != 0){
+//   arr[o++] = f;
+//    } else {
+//      continue;
+//    }
+//   }
+
+//  for (let i = 1; i <= lines; i++){ //линии
+
+//       for (let j = 0; j < arr[i-1]; j++){ //звездочки
+
+//           if (j === 0){
+//                     for (let k = 0; k <= lines - i; k++){ //пробелы
+//                        result += " ";
+//                        }
+//                     }
+//             result += "*";
+//         }
+//         result += "\n";
+// }
+// console.log(result);  
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
  "use strict";
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", " "),
-
-    personalMovieDB = {
+let numberOfFilms;
+let personalMovieDB = {
         "count" : numberOfFilms,
         "movies" : {},
         "actors" : {},
@@ -9,14 +8,67 @@ let numberOfFilms = +prompt("Сколько фильмов вы уже посм�
         "privat" : false
     };
 
-let lastFilm = prompt("Один из последних просмотренных фильмов?", " ");
-let rateFilm = prompt("Насколько оцените его?", " ");
 
-personalMovieDB.movies[lastFilm] = rateFilm;
+start();
 
-let lastFilm2 = prompt("Один из последних просмотренных фильмов?", " ");
-let rateFilm2 = prompt("Насколько оцените его?", " ");
+rememberMyFilms();
 
-personalMovieDB.movies[lastFilm2] = rateFilm2;
+writeYourGenres ();
 
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+
+
+ function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",);
+
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",);
+    }
+}
+
+ function rememberMyFilms() {
+    for (let i = 0; i < 2; i++){
+        let lastFilm = prompt("Один из последних просмотренных фильмов?",);
+        let rateFilm = prompt("Насколько оцените его?",);
+    
+        if (lastFilm !== "" &&  rateFilm !== "" && rateFilm !== null && lastFilm !== null && lastFilm.length < 50 ){
+            personalMovieDB.movies[lastFilm] = rateFilm;
+            } else {
+                i--;
+            }
+     }
+ }
+
+ function detectPersonalLevel(){
+    if (personalMovieDB["count"]<10){
+        alert("Просмотрено довольно мало фильмов");
+     } else if (10 < personalMovieDB["count"] < 30) {
+         alert("Вы классический зритель");
+     } else if (personalMovieDB["count"] > 30) {
+        alert("Вы киноман");
+     } else {
+        alert("Произошла ошибка");
+     }
+}
+
+function writeYourGenres (){
+    for (let i = 0; i < 3; i++){
+        let genre = prompt(`Ваш любимый жанр под номером ${i + 1}`,); 
+        if (genre !== "" && genre !== null && isNaN(genre)){
+            personalMovieDB.genres[i] = genre;
+        } else {
+            i--;
+        }
+    }
+    
+}
+
+function showMyDB (hidden){
+    if (!hidden){
+        console.log(personalMovieDB);
+        }
+}
+
+showMyDB (personalMovieDB.privat);
+
